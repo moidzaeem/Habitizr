942408d31f305770ee316a415b169bbe5df649ca
TMS <moidzaeem@gmail.com>
2025-04-13 02:59:18 +0500

    done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route,Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -40,6 +40,9 @@ function Router() {
     <Switch>
       <Route path="/" component={Dashboard} />
       <Route path="/profile" component={Profile} />
+      <Route path="/auth">
+        <Redirect to="/" />
+      </Route>
       <Route 
         path="/admin" 
         component={() => {
